f05c09ac9f670646bf96be233f9de034288275f5
PPUDANI <zxvk1030@naver.com>
2023-09-12 14:53:54 +0900

    조대규 : 저장 업데이트

--- Project/GameEngineContents/StartingRuins.cpp
+++ Project/GameEngineContents/StartingRuins.cpp
@@ -19,23 +19,23 @@ void StartingRuins::Start()
 
 	GlobalLoad::LoadSpriteCut(64, 64, "Underworld.png", "Resource\\Texture\\Map\\");
 	GlobalLoad::LoadSpriteCut(16, 16, "Spectiles.png", "Resource\\Texture\\Map\\");
-	//BGMap = CreateActor<TileMap>(UPDATE_ORDER::Map);
-	//BGMap->TileMapInit(60, 80, "Underworld.png");
-	//BGMap->SetTileData("Resource\\TileMapData\\SludgeHeart\\BG.tmd", 1);
-	//BGMap->Transform.SetLocalPosition({ -976.0f, 700.0f });
-	//BGMap->SetTileTexture(RENDERING_ORDER::BGMap);
+	BGMap = CreateActor<TileMap>(UPDATE_ORDER::Map);
+	BGMap->TileMapInit(60, 80, "Underworld.png");
+	BGMap->SetTileData("Resource\\TileMapData\\SludgeHeart\\BG.tmd", 1);
+	BGMap->Transform.SetLocalPosition({ -976.0f, 700.0f });
+	BGMap->SetTileTexture(RENDERING_ORDER::BGMap);
 
-	//FGMap = CreateActor<TileMap>(UPDATE_ORDER::Map);
-	//FGMap->TileMapInit(60, 80, "Underworld.png");
-	//FGMap->SetTileData("Resource\\TileMapData\\SludgeHeart\\FG.tmd", 1);
-	//FGMap->Transform.SetLocalPosition({ -976.0f, 700.0f });
-	//FGMap->SetTileTexture(RENDERING_ORDER::FGMap);
+	FGMap = CreateActor<TileMap>(UPDATE_ORDER::Map);
+	FGMap->TileMapInit(60, 80, "Underworld.png");
+	FGMap->SetTileData("Resource\\TileMapData\\SludgeHeart\\FG.tmd", 1);
+	FGMap->Transform.SetLocalPosition({ -976.0f, 700.0f });
+	FGMap->SetTileTexture(RENDERING_ORDER::FGMap);
 
-	COLMap = CreateActor<TileMap>(UPDATE_ORDER::Map);
-	COLMap->TileMapInit(60, 80, "Spectiles.png");
-	COLMap->SetTileData("Resource\\TileMapData\\SludgeHeart\\COL.tmd", 4097);
-	COLMap->Transform.SetLocalPosition({ -976.0f, 700.0f });
-	COLMap->SetTileTexture(RENDERING_ORDER::BGMap);
+	//COLMap = CreateActor<TileMap>(UPDATE_ORDER::Map);
+	//COLMap->TileMapInit(60, 80, "Spectiles.png");
+	//COLMap->SetTileData("Resource\\TileMapData\\SludgeHeart\\COL.tmd", 4097);
+	//COLMap->Transform.SetLocalPosition({ -976.0f, 700.0f });
+	//COLMap->SetTileTexture(RENDERING_ORDER::BGMap);
 
 }
 
